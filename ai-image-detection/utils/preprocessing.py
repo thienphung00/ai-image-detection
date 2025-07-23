9a767fd7de76ae554fa2417e74bc244c2c9d92f8
thienphung00 <129565001+thienphung00@users.noreply.github.com>
2025-07-23 15:12:59 -0700

    Update preprocessing.py

--- ai-image-detection/utils/preprocessing.py
+++ ai-image-detection/utils/preprocessing.py
@@ -11,4 +11,11 @@ def get_transform(model_name="resnet50"):
                              std=[0.229, 0.224, 0.225])
     ])
 
-
+def get_fft_transform():
+    return transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.CenterCrop((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],  # center-normalized
+                             std=[0.5, 0.5, 0.5])   # less biased by ImageNet stats
+    ])
