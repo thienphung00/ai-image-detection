c31e604fbdf1dd3bff45abbc057b247d1038f3c8
thienphung00 <129565001+thienphung00@users.noreply.github.com>
2025-08-11 10:53:14 -0700

    Update rearrange.py

--- ai-image-detection/preprocessing/rearrange.py
+++ ai-image-detection/preprocessing/rearrange.py
@@ -1,3 +1,59 @@
+# author: Zhiyuan Yan
+# email: zhiyuanyan@link.cuhk.edu.cn
+# date: 2023-03-29
+# description: Data pre-processing script for deepfake dataset.
+
+
+"""
+After running this code, it will generates a json file looks like the below structure for re-arrange data.
+
+{
+    "FaceForensics++": {
+        "Deepfakes": {
+            "video1": {
+                "label": "fake",
+                "frames": [
+                    "/path/to/frames/video1/frame1.png",
+                    "/path/to/frames/video1/frame2.png",
+                    ...
+                ]
+            },
+            "video2": {
+                "label": "fake",
+                "frames": [
+                    "/path/to/frames/video2/frame1.png",
+                    "/path/to/frames/video2/frame2.png",
+                    ...
+                ]
+            },
+            ...
+        },
+        "original_sequences": {
+            "youtube": {
+                "video1": {
+                    "label": "real",
+                    "frames": [
+                        "/path/to/frames/video1/frame1.png",
+                        "/path/to/frames/video1/frame2.png",
+                        ...
+                    ]
+                },
+                "video2": {
+                    "label": "real",
+                    "frames": [
+                        "/path/to/frames/video2/frame1.png",
+                        "/path/to/frames/video2/frame2.png",
+                        ...
+                    ]
+                },
+                ...
+            }
+        }
+    }
+}
+"""
+
+
 import os
 import glob
 import re
@@ -6,7 +62,6 @@ import json
 import yaml
 import pandas as pd
 from pathlib import Path
-import sys
 
 
 def generate_dataset_file(dataset_name, dataset_root_path, output_file_path, compression_level='c23', perturbation = 'end_to_end'):
@@ -693,33 +748,19 @@ def generate_dataset_file(dataset_name, dataset_root_path, output_file_path, com
     print(f"{dataset_name}.json generated successfully.")
 
 if __name__ == '__main__':
-
+    # from config.yaml load parameters
     yaml_path = './config.yaml'
+    # open the yaml file
     try:
         with open(yaml_path, 'r') as f:
             config = yaml.safe_load(f)
     except yaml.parser.ParserError as e:
         print("YAML file parsing error:", e)
-        sys.exit(1)
-    except FileNotFoundError:
-        print(f"Could not find config file at {yaml_path}")
-        sys.exit(1)
 
-    dataset_name = config['rearrange']['dataset_name'].get('default', 'FaceForensics++')
-    
-    # S3 paths from config.yaml
-    # Example: 's3://df40-sagemaker-thienphung/cdf_real'
-    dataset_root_path = config['rearrange']['dataset_root_path'].get('default')
-    output_file_path = config['rearrange']['output_file_path'].get('default', 'data/metadata/dataset.json')
-
-    comp = config['rearrange']['comp'].get('default', 'c23')
-    perturbation = config['rearrange']['perturbation'].get('default', 'end_to_end')
-
-    print(f"[INFO] Starting dataset restructuring for {dataset_name}")
-    print(f"[INFO] Dataset root path: {dataset_root_path}")
-    print(f"[INFO] Output path: {output_file_path}")
-
-    # Execute dataset rearrangement
+    dataset_name = config['rearrange']['dataset_name']['default']
+    dataset_root_path = config['rearrange']['dataset_root_path']['default']
+    output_file_path = config['rearrange']['output_file_path']['default']
+    comp = config['rearrange']['comp']['default']
+    perturbation = config['rearrange']['perturbation']['default']
+    # Call the generate_dataset_file function
     generate_dataset_file(dataset_name, dataset_root_path, output_file_path, comp, perturbation)
-
-    print("[✅] JSON metadata successfully generated.")
